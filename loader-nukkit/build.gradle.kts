67cec0985b4eff0ca554f588371e7cd2b8a512d4
splotycode <davidscandurra@gmail.com>
2022-06-28 20:14:47 +0200

    Shade loader into loader-nukkit

--- loader-nukkit/build.gradle.kts
+++ loader-nukkit/build.gradle.kts
@@ -6,6 +6,7 @@ group = "de.timolia"
 version = "1.0-SNAPSHOT"
 
 repositories {
+    gradlePluginPortal()
     mavenCentral()
 }
 
@@ -13,9 +14,16 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
     compileOnly("org.powernukkit:powernukkit:1.6.0.1-PN")
-    implementation(project(":loader"))
+    compileOnly(project(":loader"))
 }
 
+tasks.withType<Jar> {
+    configurations["compileClasspath"].forEach { file: File ->
+        if (file.absolutePath.contains("loader/build/libs/loader-")) {
+            from(zipTree(file.absoluteFile))
+        }
+    }
+}
 tasks.getByName<Test>("test") {
     useJUnitPlatform()
 }
